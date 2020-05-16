ae6882ba6d1875152aa4fb1a0da5a3c05d66ad1e
Vladyslav <Vladddd46@ukr.net>
2020-05-16 18:42:41 +0300

    mx_del_extra_spaces fixe

--- src/mx_del_extra_spaces.c
+++ src/mx_del_extra_spaces.c
@@ -1,28 +1,50 @@
-#include"libmx.h"
+#include "libmx.h"
 
 /*
  * Deletes extra whitespaces between word;
  * Word is sequence of chars separated by whitespace.
  */
-char *mx_del_extra_spaces(char *str) {
-	if(str == NULL) 
-        return NULL;
-	int len = mx_strlen(str);
-	char *a = mx_strnew(len);
-	int j = 0, i = 0;
-	while((str[i] >= 9 && str[i] <= 13) || str[i] == 32) {
-		i++;
-	}
-	for(; i < len - 1; i++) {
-		if(((str[i] >= 9 && str[i] <= 13) || str[i] == 32) ) {
-		}
-		else {
-			a[j] = str[i];
-			j++;
-		}
-	}
-	int len1 = mx_strlen(mx_strtrim(a));
-	char *result = mx_strnew(len1);
-	result = a;
-	return result;
+
+// Checks whether char c is whitespace.
+static int is_space_custom(char c) {
+    if (c == 32 
+        || c == 9 
+        || c == 10 
+        || c == 11 
+        || c == 12  
+        || c == 13) {
+        return 1;
+    }
+    return 0;
 }
+
+char *mx_del_extra_spaces(char *str) {
+    if(str == NULL) 
+        return NULL;
+    int len_str = mx_strlen(str);
+    char *str_with_des = mx_strnew(len_str);
+    int index = 0;
+    int enable = 1;
+    int i = 0;
+
+    while(str[i]) {
+        if (is_space_custom(str[i]) && enable == 1) {
+            str_with_des[index] = str[i];
+            enable = 0;
+            index++;
+        }
+        else if (is_space_custom(str[i]) == 0) {
+            str_with_des[index] = str[i];
+            index++;
+            enable = 1;
+        }
+        i++;
+    }
+    // cut string to appropriate size;
+    char *result = mx_strnew(mx_strlen(str_with_des));
+    for (int j = 0; str_with_des[j]; ++j)
+        result[j] = str_with_des[j];
+    free(str_with_des);
+    return result;
+}
+
