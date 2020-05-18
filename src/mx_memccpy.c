10893f6cae8fd6612b8664a62f1f8828d5344be3
Vladyslav <Vladddd46@ukr.net>
2020-05-18 14:17:07 +0300

    mem functions are fixe

--- src/mx_memccpy.c
+++ src/mx_memccpy.c
@@ -1,25 +1,22 @@
 #include "libmx.h"
 
-void *mx_memccpy(void *restrict dst, void *restrict src, int c, size_t n)
-{
-	unsigned long len = n;
- 	unsigned char *dst1 = (unsigned char *)dst;
+/*
+ * Has the same behaviour as standart memccpy c function.
+ */
+void *mx_memccpy(void *restrict dst, void *restrict src, int c, size_t n) {
+    unsigned long len = n;
+    unsigned char *dst1 = (unsigned char *)dst;
     unsigned char *src1 = (unsigned char *)src;
-
     unsigned long i = 0;
-    for(; i < len && src1[i - 1] != c; i++) 
-    {
-		dst1[i] = src1[i];
-	}
-
-	if (dst1[i-1] == c)
-	{
-		return &dst1[i];
-	}
-	else
-	{
-		return NULL;
-	}
+    
+    for(; i < len; i++) {
+        if (src1[i] == c) {
+            dst1[i] = src1[i];
+            return &dst1[i+1];
+        }
+        dst1[i] = src1[i];
+    }
+    return NULL;
 }
 
 
--- src/mx_memcmp.c
+++ src/mx_memcmp.c
@@ -1,24 +1,18 @@
 #include "libmx.h"
 
+/*
+ * The memcmp() function compares byte string s1 against byte string s2.
+ * Both strings are assumed to be n bytes long.
+ */
 int mx_memcmp(void *s1, void *s2, size_t n) {
     unsigned long len =(unsigned long)n;
+    unsigned char *a = (unsigned char *)s1;
+    unsigned char *b = (unsigned char *)s2;
 
-	char *a = (char *)s1;
-	char *b = (char *)s2;
-
-
-	unsigned long i = 0;
-    while(a[i] == b[i] && i < len) 
-    {   
-        i++;
+    unsigned long i = 0;
+    for (; i < len; ++i) {
+        if(a[i] != b[i]) 
+            return a[i] - b[i];
     }
-    if(a[i] != b[i]) 
-    {
-        return a[i] - b[i];
-    } 
     return 0;
 }
-
-
-
-
